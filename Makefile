75769bcc117c88dff5e613fd6d481ddf069f8f35
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2021-08-30 21:40:19 +1000

    optimisations and bug fixes

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 CC=g++
-CXXFLAGS=-I. -g -fopenmp -static-libstdc++ -O3 -pedantic -Wall
+CXX=g++
+CXXFLAGS=-I. -g -fopenmp -static-libstdc++ -O3 -fno-exceptions -march=native -funroll-loops -pedantic -Wall
 DEPS = sha512.h
 
 %.o: %.c $(DEPS)
 	$(CC) -c -o $@ $< $(CFLAGS)
 
-kseqalign: kseqalign.o
+all: kseqalign.o
 	$(CC) -o kseqalign kseqalign.o $(CXXFLAGS)
 
 .PHONY: clean
--- kseqalign.cpp
+++ kseqalign.cpp
@@ -1,14 +1,12 @@
 // CPP program to solve the sequence alignment
 // problem. Adapted from https://www.geeksforgeeks.org/sequence-alignment-problem/
-#include <sys/time.h>
+#include <chrono>
 #include <string>
 #include <cstring>
 #include <iostream>
-#include <omp.h>
+#include <cstdio>
 
-#ifdef DEBUG_HASHES
 #include "sha512.hh"
-#endif
 
 using namespace std;
 
@@ -24,20 +22,23 @@ sw::sha512::calculate(&data, sizeof(data)) // hash of any block of data
 
 // Return current wallclock time, for performance measurement
 uint64_t GetTimeStamp() {
-    struct timeval tv;
-    gettimeofday(&tv,NULL);
-    return tv.tv_sec*(uint64_t)1000000+tv.tv_usec;
+    return chrono::duration_cast<chrono::microseconds>
+            (chrono::system_clock::now().time_since_epoch())
+                .count();
 }
 
-int main(int argc, char **argv){
+int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv){
 	int misMatchPenalty;
 	int gapPenalty;
 	int k;
+    // #ifdef DEBUG_BUILD
+    // freopen("mseq-simple.dat", "r", stdin);
+    // #endif
 	std::cin >> misMatchPenalty;
 	std::cin >> gapPenalty;
 	std::cin >> k;
 
-	std::string *genes = new std::string[k];
+	auto *genes = new std::string[k];
 	for(int i=0;i<k;i++) {
 		std::cin >> genes[i];
 	}
@@ -57,7 +58,7 @@ int main(int argc, char **argv){
 	printf("Time: %ld us\n", (uint64_t) (GetTimeStamp() - start));
 
 	// print the alginment hash
-	std::cout<<alignmentHash<<std::endl;
+	std::cout  <<  "ALIGNMENT: " << alignmentHash << std::endl;
 
 	for(int i=0;i<numPairs;i++){
 		std::cout<<penalties[i] << " ";
@@ -79,84 +80,89 @@ inline int min3(int a, int b, int c) {
 // (Delete structure by  delete[] dp[0]; delete[] dp;)
 int **new2d (int width, int height)
 {
-	int **dp = new int *[width];
+	int **dp = new int*[width];
 	size_t size = width;
 	size *= height;
+    // not catching malloc error
+    // as it is not reliable due to overcommitting etc.
 	int *dp0 = new int[size];
-	if (!dp || !dp0)
-	{
-	    std::cerr << "getMinimumPenalty: new failed" << std::endl;
-	    exit(1);
-	}
 	dp[0] = dp0;
-
 	for (int i = 1; i < width; i++) {
 		dp[i] = dp[i-1] + height;
 	}
-
-
 	return dp;
 }
 
 std::string getMinimumPenalties(std::string *genes, int k, int pxy, int pgap,
 	int *penalties)
 {
+	std::string alignmentHash;
+
 	int probNum=0;
-	std::string alignmentHash="";
-	for(int i=1;i<k;i++){
-		for(int j=0;j<i;j++){
-			std::string gene1 = genes[i];
-			std::string gene2 = genes[j];
-			int m = gene1.length(); // length of gene1
-			int n = gene2.length(); // length of gene2
-			int l = m+n;
-			int *xans = new int[l+1];
-			int *yans = new int[l+1];
-			penalties[probNum]=getMinimumPenalty(gene1,gene2,pxy,pgap,xans,yans);
-			// Since we have assumed the answer to be n+m long,
-			// we need to remove the extra gaps in the starting
-			// id represents the index from which the arrays
-			// xans, yans are useful
-			int id = 1;
-			int a;
-			for (a = l; a >= 1; a--)
-			{
-				if ((char)yans[a] == '_' && (char)xans[a] == '_')
+
+	// #pragma omp parallel shared(probNum, alignmentHash)
+	{
+		// #pragma omp for collapse(2)
+		for(int i=1;i<k;i++){
+			for(int j=0;j<i;j++){
+                auto gene1 = genes[i];
+				auto gene2 = genes[j];
+				auto m = gene1.length(); // length of gene1
+				auto n = gene2.length(); // length of gene2
+				auto l = m+n;
+				int *xans = new int[l+1];
+				int *yans = new int[l+1];
+
+				penalties[probNum]=getMinimumPenalty(gene1,gene2,pxy,pgap,xans,yans);
+				// Since we have assumed the answer to be n+m long,
+				// we need to remove the extra gaps in the starting
+				// id represents the index from which the arrays
+				// xans, yans are useful
+				unsigned long id = 1;
+				unsigned long a;
+				for (a = l; a >= 1; a--)
 				{
-					id = a + 1;
-					break;
+					if ((char)yans[a] == '_' && (char)xans[a] == '_')
+					{
+						id = a + 1;
+						break;
+					}
 				}
+				std::string align1;
+				std::string align2;
+				for (a = id; a <= l; a++)
+				{
+					align1.append(1,(char)xans[a]);
+				}
+				for (a = id; a <= l; a++)
+				{
+					align2.append(1,(char)yans[a]);
+				}
+
+				std::string align1hash = sw::sha512::calculate(align1);
+				std::string align2hash = sw::sha512::calculate(align2);
+				std::string problemhash = sw::sha512::calculate(align1hash.append(align2hash));
+				// #pragma omp critical
+				{
+
+					alignmentHash=sw::sha512::calculate(alignmentHash.append(problemhash));
+
+					// Uncomment for testing purposes
+//					std::cout << penalties[probNum] << std::endl;
+//					std::cout << align1 << std::endl;
+//					std::cout << align2 << std::endl;
+//					std::cout << std::endl;
+
+
+					probNum++;
+				}
+
+				delete[] xans;
+				delete[] yans;
 			}
-			std::string align1="";
-			std::string align2="";
-			for (a = id; a <= l; a++)
-			{
-				align1.append(1,(char)xans[a]);
-			}
-			for (a = id; a <= l; a++)
-			{
-				align2.append(1,(char)yans[a]);
-			}
-
-            #ifdef DEBUG_HASHES
-			std::string align1hash = sw::sha512::calculate(align1);
-			std::string align2hash = sw::sha512::calculate(align2);
-			std::string problemhash = sw::sha512::calculate(align1hash.append(align2hash));
-			alignmentHash=sw::sha512::calculate(alignmentHash.append(problemhash));
-
-			// Uncomment for testing purposes
-			std::cout << penalties[probNum] << std::endl;
-			std::cout << align1 << std::endl;
-			std::cout << align2 << std::endl;
-			std::cout << std::endl;
-            #endif
-
-			probNum++;
-
-			delete[] xans;
-			delete[] yans;
 		}
 	}
+
 	return alignmentHash;
 }
 
@@ -167,8 +173,8 @@ int getMinimumPenalty(std::string x, std::string y, int pxy, int pgap, int * __r
 
 	int i, j; // intialising variables
 
-	int m = x.length(); // length of gene1
-	int n = y.length(); // length of gene2
+	int m = static_cast<int>(x.length()); // length of gene1
+	int n = static_cast<int>(y.length()); // length of gene2
 
 	// table for storing optimal substructure answers
 	int **dp = new2d (m+1, n+1);
@@ -176,48 +182,49 @@ int getMinimumPenalty(std::string x, std::string y, int pxy, int pgap, int * __r
 	size *= n + 1;
 	memset (dp[0], 0, size);
 
-	#pragma omp parallel
-    {
 
-		for (i = 0; i <= m; i++)
-		{
-			dp[i][0] = i * pgap;
-		}
+	for (i = 0; i <= m; i++)
+	{
+		dp[i][0] = i * pgap;
+	}
 
-		for (i = 0; i <= n; i++)
-		{
-			dp[0][i] = i * pgap;
-		}
+	for (i = 0; i <= n; i++)
+	{
+		dp[0][i] = i * pgap;
+	}
 
-		const int N = m + 1;
-		const int M = n + 1;
-		for(int i=0;i<=N+M-2; i++)
+	const int N = m + 1;
+	const int M = n + 1;
+
+	
+
+	for(i=0;i<=N+M-2; i++)
+	{
+		for(j = 0; j<=i; j++)
 		{
-			#pragma omp for simd
-			for(int j = 0; j<=i; j++)
-			{
-				int a = j, b = i-j;
-				if(i%2==0)
-					swap(a, b);
-				if(a>=N||b>=M)
-					continue;
-				if (a != 0 && b != 0) {
-					if (x[a - 1] == y[b - 1])
-					{
-						dp[a][b] = dp[a - 1][b - 1];
-					}
-					else
-					{
-						dp[a][b] = min3(dp[a - 1][b - 1] + pxy ,
-								dp[a - 1][b] + pgap ,
-								dp[a][b - 1] + pgap);
-					}
+			// std::cout << i << ", " << j << std::endl;
+			int a = j, b = i-j;
+			if(i%2==0)
+				swap(a, b);
+			if(a>=N||b>=M)
+				continue;
+			if (a != 0 && b != 0) {
+				if (x[a - 1] == y[b - 1])
+				{
+					dp[a][b] = dp[a - 1][b - 1];
+				}
+				else
+				{
+					dp[a][b] = min3(dp[a - 1][b - 1] + pxy ,
+							dp[a - 1][b] + pgap ,
+							dp[a][b - 1] + pgap);
 				}
 			}
 		}
+		
+	}
 
 
-    }
 
 	// Reconstructing the solution
 	int l = n + m; // maximum possible length
@@ -229,13 +236,7 @@ int getMinimumPenalty(std::string x, std::string y, int pxy, int pgap, int * __r
 
 	while ( !(i == 0 || j == 0))
 	{
-		if (x[i - 1] == y[j - 1])
-		{
-			xans[xpos--] = (int)x[i - 1];
-			yans[ypos--] = (int)y[j - 1];
-			i--; j--;
-		}
-		else if (dp[i - 1][j - 1] + pxy == dp[i][j])
+		if (x[i - 1] == y[j - 1] || dp[i - 1][j - 1] + pxy == dp[i][j])
 		{
 			xans[xpos--] = (int)x[i - 1];
 			yans[ypos--] = (int)y[j - 1];
