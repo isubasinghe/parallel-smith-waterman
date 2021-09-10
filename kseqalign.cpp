43a085e8218713ec4eba57c5743761eb77e74349
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2021-09-10 23:36:58 +1000

    feat: parralelised

--- kseqalign.cpp
+++ kseqalign.cpp
@@ -176,38 +176,46 @@ inline void diagonalise(int **dp, int width, int height, int di, int dj, std::st
   int outer_i = 0;
   int outer_j = 0;
 
-  for(int d = 0; d < diagonals; d++) {
-    int diff_i = outer_i;
-    int diff_j = height - outer_j -1;
+  int diff_i = 0;
+  int diff_j = 0;
+  int diag_i = 0;
+  int diag_j = 0;
+  int length = 0;
 
-    int diag_i = 1 + (diff_i / di);
-    int diag_j = 1 + (diff_j / dj);
-    int length = std::min(diag_i, diag_j);
+  {
+    for(int d = 0; d < diagonals; d++) {
+      diff_i = outer_i;
+      diff_j = height - outer_j -1;
 
-    for(int tile=0; tile < length; tile++) {
-      int inner_i = std::max(1, outer_i - (tile * di));
-      int inner_j = std::max(1, outer_j + (tile * dj));
+      diag_i = 1 + (diff_i / di);
+      diag_j = 1 + (diff_j / dj);
+      length = std::min(diag_i, diag_j);
 
-      int imax = std::min(inner_i + di, width);
-      int jmax = std::min(inner_j + dj, height);
-      for(int i = inner_i; i < imax; i++) {
-        for(int j = inner_j; j < jmax; j++) {
-          if(x[j-1] == y[i-1]) {
-            dp[j][i] = dp[j-1][i-1];
-          }else {
-            dp[j][i] = min3(dp[j-1][i-1] + pxy, dp[j-1][i] + pgap, dp[j][i-1] + pgap);
+      #pragma omp parallel for schedule(dynamic) default(none) shared(x, y, dp, di, dj, pgap, pxy, length, width, height, outer_i, outer_j)
+      for(int tile=0; tile < length; tile++) {
+        int inner_i = std::max(1, outer_i - (tile * di));
+        int inner_j = std::max(1, outer_j + (tile * dj));
+
+        int imax = std::min(inner_i + di, width);
+        int jmax = std::min(inner_j + dj, height);
+        for(int i = inner_i; i < imax; i++) {
+          for(int j = inner_j; j < jmax; j++) {
+            if(x[j-1] == y[i-1]) {
+              dp[j][i] = dp[j-1][i-1];
+            }else {
+              dp[j][i] = min3(dp[j-1][i-1] + pxy, dp[j-1][i] + pgap, dp[j][i-1] + pgap);
+            }
           }
         }
       }
-    }
 
-    if (outer_i + di < width) {
-      outer_i += di;
-    }else {
-      outer_j += dj;
+      if (outer_i + di < width) {
+        outer_i += di;
+      }else {
+        outer_j += dj;
+      }
     }
   }
-
 }
 
 // function to find out the minimum penalty
@@ -238,7 +246,7 @@ int getMinimumPenalty(std::string x, std::string y, int pxy, int pgap,
   const int N = m + 1;
   const int M = n + 1;
 
-  diagonalise(dp, M, N, 2, 2, x, y, pxy, pgap);
+  diagonalise(dp, M, N, 20, 20, x, y, pxy, pgap);
 	// Reconstructing the solution
 	int l = n + m; // maximum possible length
 
